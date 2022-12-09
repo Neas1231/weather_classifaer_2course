281a0e5212389d00a3d57f004957e046275a9052
Neas1231 <120177610+Neas1231@users.noreply.github.com>
2022-12-09 14:43:48 +0300

    Update setup_ok.py

--- setup_ok.py
+++ setup_ok.py
@@ -14,7 +14,16 @@ setuptools.setup(
     classifiers=[],
     python_requires=">=3.6",
     install_requires=[
-        "tensorflow >= 2.11.0",
+        "flask==2.0.0",
+        "numpy==1.17.4",
+        "tensorflow==1.15.2",
+        "opencv-python-headless",
+        "werkzeug==0.16.0",
+        "matplotlib==3.1.1",
+        "gunicorn==19.5.0",
+        "Pillow==6.0.0",
+        "image",
+        "scipy",
         "streamlit >= 0.63",
         "PIL",
         "st_on_hover_tabs"
