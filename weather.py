1934e652acd989c7a99f65761109a32990e9080b
Neas1231 <nicka-rudnev@mail.ru>
2022-12-20 09:51:18 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -43,7 +43,10 @@ if uploaded_file is not None:
 
        with st.spinner('Подождите...'):
               preds = ResNet_model.predict(image)
-       st.success('Выполнено!')  
+       st.success('Выполнено!')
+
+       with st.echo():
+              st.write(dictuar[preds.argmax()])  
 
        with st.sidebar:
                tabs = on_hover_tabs(tabName=['Предсказание', 'Картинка', 'Все вместе'], 
